42aafc2d954773f2079aabbef9c9ca60cee59a7b
Flavio Augusto Oliveira Costa <flavio1@ice.ufjf.br>
2022-08-01 21:44:28 -0300

    Criado constantes para elementos

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+const sheep = document.getElementById('Sheep');
+const wolf = document.getElementById("Wolf");
+const lettuce = document.getElementById("Lettuce");
+const farmer = document.getElementById("Farmer");
+const boat = document.getElementById("Boat");
