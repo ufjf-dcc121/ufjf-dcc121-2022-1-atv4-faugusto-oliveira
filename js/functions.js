4e7e27be23e8c6f7f4ad67c721baab5389df3867
Flavio Augusto Oliveira Costa <flavio1@ice.ufjf.br>
2022-08-01 21:45:36 -0300

    Criado eventsListeners e funcao moveElements

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,11 @@ const wolf = document.getElementById("Wolf");
 const lettuce = document.getElementById("Lettuce");
 const farmer = document.getElementById("Farmer");
 const boat = document.getElementById("Boat");
+
+sheep.addEventListener("click", moveElement);
+wolf.addEventListener("click", moveElement);
+lettuce.addEventListener("click", moveElement);
+
+function moveElement(){
+	
+}
